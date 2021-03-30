3045ec6fedc3c53fc049962439b4565352e770ca
Vivek205 <ichbinvivek@gmail.com>
2021-03-30 09:40:44 +0530

    moved config to the server

--- aiService.js
+++ aiService.js
@@ -1,7 +1,6 @@
-
 /**
-* dotenv must be on the top of the entry file of the project
-*/
+ * dotenv must be on the top of the entry file of the project
+ */
 import dotenv from "dotenv";
 import SnetSDK, { DefaultPaymentStrategy } from "@vivek205/node-sdk-dev";
 /**
@@ -14,13 +13,17 @@ import config from "./config";
 dotenv.config();
 const sdk = new SnetSDK(config);
 
-const aiService = async () => {
+const aiService = async (content = "", style = "") => {
   const orgId = "snet";
   const serviceId = "style-transfer";
   const groupName = "default_group";
   const paymentStrategy = new DefaultPaymentStrategy(2);
   let tokenToMakeFreeCall = process.env.FREE_CALL_TOKEN ? process.env.FREE_CALL_TOKEN.toUpperCase() : "";
-  tokenToMakeFreeCall = Boolean(tokenToMakeFreeCall) ? (tokenToMakeFreeCall.startsWith("0X") ? tokenToMakeFreeCall : `0X${tokenToMakeFreeCall}`) : "";
+  tokenToMakeFreeCall = Boolean(tokenToMakeFreeCall)
+    ? tokenToMakeFreeCall.startsWith("0X")
+      ? tokenToMakeFreeCall
+      : `0X${tokenToMakeFreeCall}`
+    : "";
   const serviceClientOptions = {
     tokenToMakeFreeCall,
     tokenExpirationBlock: process.env.TOKEN_EXPIRATION_BLOCK,
@@ -35,43 +38,41 @@ const aiService = async () => {
 
   try {
     const serviceClient = await sdk.createServiceClient(
-        orgId,
-        serviceId,
-        service.StyleTransferClient,
-        groupName,
-        paymentStrategy,
-        serviceClientOptions
-      );
-      console.log("created service client");
-      /**
-       * 2: Initialize the request object and the set the required input values
-       */
-      const request = new messages.TransferImageStyleRequest();
-      request.setContent("https://cdn.analyticsvidhya.com/wp-content/uploads/2020/04/featured_image-1.jpg");
-      request.setStyle("https://miro.medium.com/max/5000/1*1BUIofZgqVuR6nj8LbrRtQ.jpeg");
-      request.setContentsize(640);
-      request.setStylesize(640);
-      request.setPreservecolor(false);
-      request.setAlpha(1);
-      request.setCrop(false);
-      request.setSaveext("");
-      
-      return new  Promise((resolve, reject)=> {
-        /**
-       * 3: Change the method name according to your service 
-       */
-        serviceClient.service.transfer_image_style(request, (err, result) => {
-            if (err) {
-              return reject(err);
-            }
-            resolve(result);
-          });
-      })
+      orgId,
+      serviceId,
+      service.StyleTransferClient,
+      groupName,
+      paymentStrategy,
+      serviceClientOptions
+    );
+    console.log("created service client");
+    /**
+     * 2: Initialize the request object and the set the required input values
+     */
+    const request = new messages.TransferImageStyleRequest();
+    request.setContent(content);
+    request.setStyle(style);
+    request.setContentsize(640);
+    request.setStylesize(640);
+    request.setPreservecolor(false);
+    request.setAlpha(1);
+    request.setCrop(false);
+    request.setSaveext("");
 
+    return new Promise((resolve, reject) => {
+      /**
+       * 3: Change the method name according to your service
+       */
+      serviceClient.service.transfer_image_style(request, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
   } catch (error) {
     console.log("promise error", error);
     closeConnection();
   }
 };
 export default aiService;
-
